f5abec465b5a43385cba251cf9053435c107df3d
beskorovainyi <beskorovainyi.max@gmail.com>
2022-06-12 23:53:47 +0300

    CREATED: created board

---
+++ src/components/CellComponent/CellComponent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Cell} from "../../models/Cell";
+
+interface CellProps {
+    cell: Cell
+}
+
+const CellComponent: FC<CellProps> = ({cell: Cell}) => {
+
+    return(
+        <div className={['cell', cell.color].join(' ')}>
+
+        </div>
+    )
+}
+
+export default CellComponent
